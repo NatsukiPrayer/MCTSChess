Changed formula, small fixes, changed tree draw from plot to file, added bars for all processes

BetaZero.py
@@ -4,6 +4,7 @@ import numpy as np
 from MCTS import MCTS
 import torch
 from ChessGame import ChessGame
+from tqdm import tqdm
 import torch.nn.functional as F
 
 class BetaZero:
@@ -21,13 +22,14 @@ class BetaZero:
 
         state = state * -1
         isTerminal = False
+        idx = 0
         while True:
-            print(board)
+            # print(board)
             neutralState = state * -1
-            actionProbs = self.mcts.search(neutralState, board)
+            actionProbs = self.mcts.search(neutralState, board, idx)
             if not isTerminal:
                 memory.append((neutralState, actionProbs))
-                action = np.random.choice(self.game.actionSize, p=actionProbs)
+                action = np.argmax(actionProbs)
                 state, board = self.game.getNextState(state, action, board)
                 value, isTerminal = self.game.getValAndTerminate(board)
             if isTerminal:
@@ -38,6 +40,7 @@ class BetaZero:
                     returnMemory.append((self.game.getEncodedState(histNeutralState), histActionProbs, histOutcome))
                 return returnMemory
             player = not player
+            idx += 1
 
 
     def train(self, memory):
@@ -59,15 +62,18 @@ class BetaZero:
 
 
     def learn(self):
-        for iteration in range(self.args['numIterations']):
+        for iteration in (numIterations := tqdm(range(self.args['numIterations']), leave=False)):
+            numIterations.set_description("Iterations")
             memory = []
 
             self.model.eval()
-            for selfPlayIteration in range(self.args['numSelfPlayIterations']):
+            for selfPlayIteration in (numSelfPlayIterations := tqdm(range(self.args['numSelfPlayIterations']), leave=False)):
+                numSelfPlayIterations.set_description("Self plays")
                 memory += self.selfPlay()
 
             self.model.train()
-            for _ in range(self.args['numEpochs']):
+            for _ in (numEpochs:= tqdm(range(self.args['numEpochs']), leave=False)):
+                numEpochs.set_description("Epochs")
                 self.train(memory)
             torch.save(self.model.state_dict(), f"model_{iteration}.pt")
             torch.save(self.optimizer.state_dict(), f"optimizer_{iteration}.pt")

MCTS.py
@@ -2,11 +2,11 @@ import math
 from ChessGame import ChessGame
 import numpy as np
 import chess
-from progress.bar import Bar
 from NN import ResNet
 import torch
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Button
+from PrettyPrint import PrettyPrintTree
+from tqdm import tqdm
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
 def encode(inp: str):
@@ -18,14 +18,12 @@ def encode(inp: str):
     return rowFrom + rowWhere + colFrom + colWhere
 
 class Node:
-    def __init__(self, game: ChessGame, args, state, board: chess.Board, parent=None, actionTaken = None, prior=0, wins: int = 0, prob: int = 1):
+    def __init__(self, game: ChessGame, args, state, board: chess.Board, parent=None, actionTaken = None, prior=0):
         self.game = game
         self.args = args
         self.state = state
         self.board = board
         self.parent = parent
-        self.wins = wins
-        self.prob = prob
         
 
         if isinstance(actionTaken, int) or actionTaken is None:
@@ -39,6 +37,10 @@ class Node:
 
         self.visitCount = 0
         self.valueSum = 0
+    
+    @property
+    def val(self):
+        return f"{str(self.board)}\nVisits: {self.visitCount}\nValue: {self.valueSum}"
 
     def isFullyExpanded(self):
         return len(self.children) > 0
@@ -54,47 +56,32 @@ class Node:
         return bestChild
     
     def getUCB(self, child):
-        qValue = 0
-        if child.visitCount != 0:
-            qValue = child.wins / child.visitCount
-        
-        qValue += child.prob * self.args['C'] * math.sqrt(self.visitCount) / (1 + child.visitCount)
-        return qValue
-        # if child.visitCount == 0:
-        #     qValue = 0
-        # else:
-        #     qValue = 1 - (child.valueSum / child.visitCount + 1) / 2
-        # return qValue + self.args['C'] * ((math.sqrt(self.visitCount) / (child.visitCount + 1))) * child.prior
+        if child.visitCount == 0:
+            qValue = 0
+        else:
+            qValue = 1 - (child.valueSum / child.visitCount + 1) / 2
+        return qValue + self.args['C'] * ((math.sqrt(self.visitCount) / (child.visitCount + 1))) * child.prior
 
 
-    def expand(self, policy):
+    def  expand(self, policy):
         for action, prob in enumerate(policy):
             if prob > 0:
                 childState = self.state.copy()
                 childBoard = self.board.copy()
                 childState, childBoard  = self.game.getNextState(childState, action, childBoard)
-                if childBoard.is_checkmate():
-                    self.wins += 1 
                 childState = self.game.changePerspective(childState)
-                child = Node(self.game, self.args, childState, childBoard, self, action, wins = 0, prob=prob)
+                child = Node(self.game, self.args, childState, childBoard, self, action, prob)
                 self.children.append(child)
         return self
     
         
-    def backpropogate(self, value, wins):
+    def backpropogate(self, value):
         self.valueSum += value
         self.visitCount += 1
 
-        if wins == 0:
-            wins = self.wins
-        elif wins > 0:
-            self.wins += wins
-
         value *= -1
-        wins *= -1
         if self.parent is not None:
-            self.parent.backpropogate(value, wins)
-
+            self.parent.backpropogate(value)
 
 class Drawer:
     def __init__(self, root: Node):
@@ -105,31 +92,10 @@ class Drawer:
         self.root = root
 
     def update(self, x: float = 0.5, y: float = 0.5):
-        plt.clf()
-        self.maxX = 0
-        self.minY = 0
-        self.__updater(self.root, x, y)
-        plt.xlim(x - self.xStep, self.maxX + self.xStep)
-        plt.ylim(self.minY + self.yStep, y - self.yStep)
-        # plt.rcParams["figure.autolayout"] = True
-        plt.show()
-        
-
-    def __updater(self, parent: Node, x: float, y:float):
-        minX = x
-        for node in parent.children:
-            x = self.__updater(node, x + self.xStep, y + self.yStep)
-            if x > self.maxX:
-                self.maxX = x
-            if y < self.minY:
-                self.minY = y
-        self.__draw(parent, (x + minX)/2, y)
-        return x
-        
-
-    def __draw(self, node: Node, x: float, y: float):
-        plt.text(x,y, f"{str(node.board)}\nVisits: {node.visitCount}\nWins: {node.wins}\nPolicy: {node.prob}\n Value: {node.valueSum}", ha='center', va='center', bbox=dict(facecolor='lightblue', edgecolor='black'))
-
+        pt = PrettyPrintTree(lambda x: x.children, lambda x: x.val, max_depth=2, return_instead_of_print=True)
+        tree_as_str = pt(self.root)
+        with open('tree.txt', 'w', encoding="utf-8") as f:
+            f.write(tree_as_str.encode("utf-8").decode('utf-8'))
 
         
 class MCTS:
@@ -140,38 +106,38 @@ class MCTS:
         self.drawer = None
 
     @torch.no_grad()
-    def search(self, state, board):
-        root = Node(self.game, self.args, state, board, prob = 1)
+    def search(self, state, board, idx):
+        root = Node(self.game, self.args, state, board, prior = 1)
         self.drawer = Drawer(root)
-        with Bar('Processing...', max=self.args['num_searches']) as bar:
+
             
-            for _ in range(self.args['num_searches']):
-                # self.drawer.update()
-                node = root
-                while node.isFullyExpanded():
-                    node = node.select()
+        for _ in (num_searches := tqdm(range(self.args['num_searches']), leave=False)):
+            # self.drawer.update()
+            num_searches.set_description(f"Searches {idx}")
+            node = root
+            while node.isFullyExpanded():
+                node = node.select()
 
-                value, isTerminal = self.game.getValAndTerminate(node.board)
-                value = value * -1 
+            value, isTerminal = self.game.getValAndTerminate(node.board)
+            value = value * -1 
 
-                if not isTerminal:
-                    policy, value = self.model(torch.tensor(self.game.getEncodedState(node.state)).unsqueeze(0))
-                    policy = torch.softmax(policy, axis=1).squeeze(0).cpu().numpy()
-                    validMoves = self.game.getValidMoves(node.board)
-                    zeros = np.zeros(4096)
-                    for move in validMoves:
-                        zeros[encode(str(move))] = 1
-                    policy *= zeros
-                    policy /= np.sum(policy)
+            if not isTerminal:
+                policy, value = self.model(torch.tensor(self.game.getEncodedState(node.state)).unsqueeze(0))
+                policy = torch.softmax(policy, axis=1).squeeze(0).cpu().numpy()
+                validMoves = self.game.getValidMoves(node.board)
+                zeros = np.zeros(4096)
+                for move in validMoves:
+                    zeros[encode(str(move))] = 1
+                policy *= zeros
+                policy /= np.sum(policy)
 
-                    value = value.item()
+                value = value.item()
 
-                    node = node.expand(policy)
+                node = node.expand(policy)
 
-                node.backpropogate(value, 0)
-                bar.next()
-        
-        self.drawer.update()
+            node.backpropogate(value)
+
+        # self.drawer.update()
         actionProbs = np.zeros(self.game.actionSize)
         if len(root.children) == 0:
             raise Exception("GameEnd???")

main.py
@@ -8,7 +8,7 @@ from BetaZero import BetaZero
 from NN import ResNet
 np.set_printoptions(threshold=sys.maxsize)
 
-args = {'C':2, 'num_searches':250, 'numIterations':1, 'numSelfPlayIterations':50, 'numEpochs':32, 'batchSize':128}
+args = {'C':2, 'num_searches':250, 'numIterations':1, 'numSelfPlayIterations':5, 'numEpochs':4, 'batchSize':128}
 
 
 chessGame = ChessGame()