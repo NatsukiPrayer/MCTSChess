Fixed some bugs, added parallel computation

BetaZeroParallel.py
@@ -1,3 +1,5 @@
+from MCTSParallel import MCTSParallel
+from SPG import SPG
 
 import random
 import numpy as np
@@ -10,43 +12,55 @@ import torch.nn.functional as F
 import time as t
 device = 'cuda'
 
-class BetaZero:
+class BetaZeroParallel:
     def __init__(self, model, optimizer, game: ChessGame, args):
         self.model = model
         self.optimizer = optimizer
         self.game = game
         self.args = args
-        self.mcts = MCTS(model, game, args)
+        self.mcts = MCTSParallel(model, game, args)
 
     def selfPlay(self):
-        memory = []
+        returnMemory = []
         player = True
-        state, board = self.game.getInitialState()
+        spGames = [SPG(self.game) for _ in range(self.args['numParallelGames'])]
 
-        state = state * -1
-        isTerminal = False
-        idx = 0
+
         tqdm.write('New game started\n')
-        while True:
-            tqdm.write(f'{str(board)}\n')
-            tqdm.write(f'{state}\n')
-            # print(board)
-            neutralState = state * -1
-            actionProbs = self.mcts.search(neutralState, board, idx)
-            if not isTerminal:
-                memory.append((neutralState, actionProbs))
+        idx = 0
+        while len(spGames) > 0:
+            states = np.stack([spg.state for spg in spGames])
+            boards = [spg.board for spg in spGames]
+            neutralStates = states * -1
+            self.mcts.search(neutralStates, boards, idx, spGames)
+
+            for i in (numGames := tqdm(range(len(spGames))[::-1], leave=False)):
+                numGames.set_description('Game {idx}')
+                spg = spGames[i]
+
+                actionProbs = np.zeros(self.game.actionSize)
+                if len(spg.root.children) == 0:
+                    raise Exception("GameEnd???")
+                for child in spg.root.children:
+                    actionProbs[child.actionTaken] = child.visitCount
+                a = np.sum(actionProbs)
+                actionProbs /= a
+                spg.memory.append((spg.root.state, actionProbs))
                 action = np.random.choice(self.game.actionSize, p=actionProbs)
-                state, board = self.game.getNextState(state, action, board)
-                value, isTerminal = self.game.getValAndTerminate(board)
-            if isTerminal:
-                returnMemory = []
-                for idx, tup in enumerate(memory):
-                    histNeutralState, histActionProbs = tup
-                    histOutcome = value if idx % 2 == 0 else -value
-                    returnMemory.append((self.game.getEncodedState(histNeutralState), histActionProbs, histOutcome))
-                return returnMemory
+                spgState, board = self.game.getNextState(spg.state, action, spg.board)
+                value, isTerminal = self.game.getValAndTerminate(spg.board)
+                tqdm.write(f'{str(board)}\n')
+                
+                if isTerminal:
+                    returnMemory = []
+                    for idx, tup in enumerate(spg.memory):
+                        histNeutralState, histActionProbs = tup
+                        histOutcome = value if idx % 2 == 0 else -value
+                        returnMemory.append((self.game.getEncodedState(histNeutralState), histActionProbs, histOutcome))
+                    del spGames[i]
             player = not player
             idx += 1
+        return returnMemory
 
 
     def train(self, memory, train=True):

MCTS.py
@@ -8,7 +8,7 @@ from PrettyPrint import PrettyPrintTree
 from tqdm import tqdm
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-device = 'cpu'
+device = 'cuda'
 def encode(inp: str):
     action = str(inp)
     rowFrom = (int(action[1]) - 1) 
@@ -24,6 +24,10 @@ class Node:
     def ucb(self):
         return self._ucb
 
+    @ucb.setter
+    def ucb(self, value):
+        self._ucb = value
+
 
     def updateUCB(self, value, idx, search):
         self._ucb = value
@@ -32,12 +36,20 @@ class Node:
         self.lastUpdate = search
         if (not self.parent is None):
             while idx < len(self.parent.children)-1:
-                self.parent.children[idx + 1].updateUCB(self.getUCB_Self(), idx+1, search)
-                if (self.parent.children[idx+1].ucb > self._ucb):
+                value = self.parent.children[idx+1].getUCB_Self()
+
+                if (value > self._ucb):
+                    self.parent.children[idx + 1].updateUCB(value, idx+1, search)
                     self.parent.children[idx], self.parent.children[idx+1] = self.parent.children[idx+1], self.parent.children[idx]
                     idx += 1
                 else:
+                    self.parent.children[idx+1].ucb = value
+                    self.parent.children[idx+1].lastSearch = search
+                    
                     break
+    
+
+     
 
     def __init__(self, game: ChessGame, args, state, board: chess.Board, parent=None, actionTaken = None, prior=0):
         self.game = game
@@ -63,21 +75,25 @@ class Node:
     
     @property
     def val(self):
-        return f"{str(self.board)}\nVisits: {self.visitCount}\nValue: {self.valueSum}"
+        try:
+            v = self.valueSum.item()
+        except:
+            v = self.valueSum
+        return f"{str(self.board)}\nVisits: {self.visitCount}\nValue: {v:.3f}" #TODO
 
     def isFullyExpanded(self):
         return len(self.children) > 0
 
     def select(self):
-        return self.children[0]
-        # bestChild = None
-        # bestUCB = -np.inf
-        # for child in self.children:
-        #     ucb = self.getUCB(child)
-        #     if ucb > bestUCB:
-        #         bestChild = child
-        #         bestUCB = ucb
-        # return bestChild
+        # return self.children[0]
+        bestChild = None
+        bestUCB = -np.inf
+        for child in self.children:
+            ucb = self.getUCB(child)
+            if ucb > bestUCB:
+                bestChild = child
+                bestUCB = ucb
+        return bestChild
     
     def getUCB(self, child):
         if child.visitCount == 0:
@@ -90,7 +106,7 @@ class Node:
         if self.visitCount == 0:
             qValue = 0
         else:
-            qValue = 1 - (self.valueSum / self.visitCount + 1) / (2 * math.sqrt(self.parent.visitCount) + 1)
+            qValue = 1 - (self.valueSum / self.visitCount + 1) / (2 * math.sqrt(self.parent.visitCount+1))
         return qValue + self.args['C'] * ((1 / (self.visitCount + 1))) * self.prior
 
 
@@ -102,8 +118,8 @@ class Node:
                 childState, childBoard  = self.game.getNextState(childState, action, childBoard)
                 childState = self.game.changePerspective(childState)
                 child = Node(self.game, self.args, childState, childBoard, self, action, prob)
-                self.children.insert(0, child)
-                child.updateUCB(child.getUCB_Self(), 0, search)
+                # self.children.insert(0, child)
+                # child.updateUCB(child.getUCB_Self(), 0, search)
 
         return self
     
@@ -114,7 +130,7 @@ class Node:
 
         value *= -1
         if self.parent is not None:
-            self.updateUCB(self.getUCB_Self(), 0, search)
+            # self.updateUCB(self.getUCB_Self(), 0, search)
             self.parent.backpropogate(value, search)
 
 class Drawer:
@@ -126,9 +142,10 @@ class Drawer:
         self.texts = []
         self.root = root
 
-    def update(self, x: float = 0.5, y: float = 0.5):
-        pt = PrettyPrintTree(lambda x: [x for x in x.children if x.visitCount > 0], lambda x: x.val, max_depth=-1, return_instead_of_print=True, color=None)
-        tree_as_str = pt(self.root)
+    def update(self, node):
+        
+        pt = PrettyPrintTree(lambda x: [y for y in x.children if y.visitCount > 0], lambda x: x.val, max_depth=-1, return_instead_of_print=True, color=None)
+        tree_as_str = pt(node)
         # with open('tree.txt', 'w', encoding="utf8") as f:
         #     f.write(tree_as_str)
         from PIL import Image
@@ -136,15 +153,16 @@ class Drawer:
         from PIL import ImageFont
         img = Image.new('RGB', (100, 100))
         d = ImageDraw.Draw(img)
-        font = ImageFont.truetype("FreeMono.ttf", size=20)
+        fontSize = 20 
+        font = ImageFont.truetype("FreeMono.ttf", size=fontSize)
 
         _, _, width, height = d.textbbox(text=tree_as_str, font=font, xy=(0,0))
-        print(width, height)
-        img = Image.new('RGB', (width, height))
+        img = Image.new('RGB', (width+2*fontSize, height+2*fontSize))
         d = ImageDraw.Draw(img)
         d.text((20, 20), tree_as_str, fill=(255, 255, 255), font=font)
         img.show()
         img.save('tree.tiff')
+        exit()
 
         
 class MCTS:
@@ -169,9 +187,8 @@ class MCTS:
 
             value, isTerminal = self.game.getValAndTerminate(node.board)
             value = value * -1 
-
             if not isTerminal:
-                policy, value = self.model(torch.tensor(self.game.getEncodedState(node.state)).unsqueeze(0))
+                policy, value = self.model(torch.tensor(self.game.getEncodedState(node.state), device = 'cuda:0').unsqueeze(0))
                 policy = torch.softmax(policy, axis=1).squeeze(0).cpu().numpy()
                 validMoves = self.game.getValidMoves(node.board)
                 zeros = np.zeros(4096)
@@ -188,7 +205,7 @@ class MCTS:
 
             node.backpropogate(value, iter)
 
-        # self.drawer.update()
+        # self.drawer.update(root)
         actionProbs = np.zeros(self.game.actionSize)
         if len(root.children) == 0:
             raise Exception("GameEnd???")

NN.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
-device = 'cpu'
+device = 'cuda'
 
 class ResNet(nn.Module):
     def __init__(self, game, numResBlocks, numHidden):
@@ -10,7 +10,7 @@ class ResNet(nn.Module):
         self.backBone = nn.ModuleList([ResBlock(numHidden) for i in range(numResBlocks)]).to(f'{device}')
         self.policyHead = nn.Sequential(nn.Conv2d(numHidden, 32, kernel_size=3, padding=1), nn.BatchNorm2d(32), nn.ReLU(), nn.Flatten(), nn.Linear(32*game.rowCount*game.colCount, game.actionSize)).to(f'{device}')
         self.valueHead = nn.Sequential(nn.Conv2d(numHidden, 13, kernel_size=3, padding=1), nn.BatchNorm2d(13), nn.ReLU(), nn.Flatten(), nn.Linear(13*game.rowCount*game.colCount, 1), nn.Tanh()).to(f'{device}')
-        self.to('cpu')
+        self.to('cuda')
     
     def forward(self, x):
         x = x.to(f'{device}')

main.py
@@ -1,4 +1,5 @@
 import sys
+from BetaZeroParallel import BetaZeroParallel
 from ChessGame import ChessGame
 
 from MCTS import MCTS
@@ -18,7 +19,7 @@ with open("config.json", "r") as f:
 
 chessGame = ChessGame()
 model = ResNet(chessGame, 16, 64)
-model.load_state_dict(torch.load('model_1.pt'))
+model.load_state_dict(torch.load('model_834.pt'))
 # model.eval()
 
 
@@ -26,8 +27,8 @@ model.load_state_dict(torch.load('model_1.pt'))
 player = True
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-optimizer.load_state_dict(torch.load('optimizer_1.pt'))
-betaZero = BetaZero(model, optimizer, chessGame, args)
+optimizer.load_state_dict(torch.load('optimizer_834.pt'))
+betaZero = BetaZeroParallel(model, optimizer, chessGame, args)
 betaZero.learn()
 
 mcts = MCTS( model, chessGame, args)